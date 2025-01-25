641efad06b1a6b32d4ebab506e7e95ea5701bca9
fcoyoca <fcoyoca@gmail.com>
2025-01-25 21:11:23 +0800

    chore: register syncfusion components

--- apps/blazor/client/Program.cs
+++ apps/blazor/client/Program.cs
@@ -2,6 +2,7 @@ using budget_request_app.Blazor.Client;
 using budget_request_app.Blazor.Infrastructure;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cWWdCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdmWX5cdXRXRGJYUEd2XkQ=");
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
