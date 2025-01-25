a26f35b10ba225a5b24b0f80e6f27ea2cb565757
fcoyoca <fcoyoca@gmail.com>
2025-01-25 13:58:26 +0800

    chore: rename fullstackhero to appname in metricconstants

--- aspire/service-defaults/MetricsConstants.cs
+++ aspire/service-defaults/MetricsConstants.cs
@@ -1,7 +1,7 @@
 ﻿namespace budget_request_app.Aspire.ServiceDefaults;
 public static class MetricsConstants
 {
-    public const string AppName = "fullstackhero";
+    public const string AppName = "budget-request-app";
     public const string Todos = "Todos";
     public const string Catalog = "Catalog";
 }
