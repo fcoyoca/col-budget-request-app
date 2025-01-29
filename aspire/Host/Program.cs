d97520ffcfd0a5ad0ce22f24df07243869a95dfa
Reindel <rjxirctt@gmail.com>
2025-01-29 09:07:47 +0800

    test deploy

--- aspire/Host/Program.cs
+++ aspire/Host/Program.cs
@@ -2,7 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddProject<Projects.Server>("webapi")
        .WithExternalHttpEndpoints()
-       .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName);;
+       .WithEnvironment("ASPNETCORE_ENVIRONMENT", builder.Environment.EnvironmentName);
 
 using var app = builder.Build();
 
