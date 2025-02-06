b73121f430ebe40e16c26d8b5ca9f181f41638bc
Reindel <rjxirctt@gmail.com>
2025-02-06 13:55:54 +0800

    lookupvalues: mapping for return

--- api/modules/LookupValue/Features/GetList/v1/GetLookupValueListHandler.cs
+++ api/modules/LookupValue/Features/GetList/v1/GetLookupValueListHandler.cs
@@ -2,13 +2,15 @@
 using FSH.Framework.Core.Persistence;
 using FSH.Framework.Core.Specifications;
 using budget_request_app.WebApi.LookupValue.Domain;
+using budget_request_app.WebApi.LookupCategory.Domain;
 using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace budget_request_app.WebApi.LookupValue.Features.GetList.v1;
 
 public sealed class GetLookupValueListHandler(
-    [FromKeyedServices("lookupValues")] IReadRepository<LookupValueItem> repository)
+    [FromKeyedServices("lookupValues")] IReadRepository<LookupValueItem> repository, 
+    [FromKeyedServices("lookupCategories")] IReadRepository<LookupCategoryItem> repositoryParent)
     : IRequestHandler<GetLookupValueListRequest, PagedList<LookupValueDto>>
 {
     public async Task<PagedList<LookupValueDto>> Handle(GetLookupValueListRequest request, CancellationToken cancellationToken)
@@ -17,7 +19,17 @@ public sealed class GetLookupValueListHandler(
 
         var spec = new EntitiesByPaginationFilterSpec<LookupValueItem, LookupValueDto>(request.Filter);
 
+        var categories = await repositoryParent.ListAsync();
+
         var items = await repository.ListAsync(spec, cancellationToken).ConfigureAwait(false);
+
+        foreach (var item in items)
+        {
+            var catName = categories.First(x => x.Id == item.LookupCategoryId).Name;
+            var propCatName = item.GetType().GetProperty("LookupCategoryName");
+            propCatName.SetValue(item, catName);
+        }
+
         var totalCount = await repository.CountAsync(spec, cancellationToken).ConfigureAwait(false);
 
         return new PagedList<LookupValueDto>(items, request.Filter.PageNumber, request.Filter.PageSize, totalCount);
