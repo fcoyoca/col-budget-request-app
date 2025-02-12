2c364d41f1ee335cac6a8a184634fa2f8e565404
Reindel <rjxirctt@gmail.com>
2025-02-12 20:44:31 +0800

    lookupValues: update name

--- api/modules/LookupValue/Features/Update/v1/UpdateLookupValueHandler.cs
+++ api/modules/LookupValue/Features/Update/v1/UpdateLookupValueHandler.cs
@@ -16,9 +16,10 @@ public sealed class UpdateLookupValueHandler(
         ArgumentNullException.ThrowIfNull(request);
         var LookupValue = await repository.GetByIdAsync(request.Id, cancellationToken);
         _ = LookupValue ?? throw new LookupValueItemNotFoundException(request.Id);
-        var updatedLookupValue = LookupValue.Update(request.Name);
-        await repository.UpdateAsync(updatedLookupValue, cancellationToken);
-        logger.LogInformation("LookupValue item updated {LookupValueItemId}", updatedLookupValue.Id);
-        return new UpdateLookupValueResponse(updatedLookupValue.Id);
+        //var updatedLookupValue = LookupValue.Update(request.Name);
+        LookupValue.Name = request.Name;
+        await repository.UpdateAsync(LookupValue, cancellationToken);
+        logger.LogInformation("LookupValue item updated {LookupValueItemId}", LookupValue.Id);
+        return new UpdateLookupValueResponse(LookupValue.Id);
     }
 }
