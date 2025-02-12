8229ff5bebc82da9cc72454c928be0a8a7e7aff6
Reindel <rjxirctt@gmail.com>
2025-02-12 17:24:54 +0800

    lookupValue: default value guid

--- api/modules/LookupValue/Features/Create/v1/CreateLookupValueCommand.cs
+++ api/modules/LookupValue/Features/Create/v1/CreateLookupValueCommand.cs
@@ -4,7 +4,7 @@ using MediatR;
 namespace budget_request_app.WebApi.LookupValue.Features.Create.v1;
 public record CreateLookupValueCommand(
     [property: DefaultValue("Hello World!")] string Name,
-    [property: DefaultValue("Important Note.")] Guid LookupCategoryId) : IRequest<CreateLookupValueResponse>;
+    Guid LookupCategoryId) : IRequest<CreateLookupValueResponse>;
 
 
 
