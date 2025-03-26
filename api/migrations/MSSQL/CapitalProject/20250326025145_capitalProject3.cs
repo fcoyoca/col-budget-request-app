e86713e439fd9e7a0fbc41e3d82bf592d5bbba73
Reindel <rjxirctt@gmail.com>
2025-03-26 10:52:16 +0800

    chore: capital projects migration scripts

---
+++ api/migrations/MSSQL/CapitalProject/20250326025145_capitalProject3.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_request_app.WebApi.Migrations.MSSQL.CapitalProject
+{
+    /// <inheritdoc />
+    public partial class capitalProject3 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "TIFFundingIds",
+                schema: "capitalProject",
+                table: "CapitalProjects",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TIFFundingIds",
+                schema: "capitalProject",
+                table: "CapitalProjects");
+        }
+    }
+}
