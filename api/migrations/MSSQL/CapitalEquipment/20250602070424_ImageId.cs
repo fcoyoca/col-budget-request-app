a6653ffc027c300d9161f72deaaa4fe8eae04357
Kimper <k.sevilla@tritontek.ph>
2025-06-04 12:06:52 +0800

    refactor(capital equipment) added column, migration

---
+++ api/migrations/MSSQL/CapitalEquipment/20250602070424_ImageId.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace budget_request_app.WebApi.Migrations.MSSQL.CapitalEquipment
+{
+    /// <inheritdoc />
+    public partial class ImageId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageId",
+                schema: "capitalEquipment",
+                table: "CapitalEquipments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageId",
+                schema: "capitalEquipment",
+                table: "CapitalEquipments");
+        }
+    }
+}
